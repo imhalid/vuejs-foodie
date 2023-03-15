3c4bab2f3b5f576b535f311219cfc978b27c4188
imhalid <imhalid@icloud.com>
2023-03-15 06:56:49 +0300

    feat: prettierrc

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: () => import('../views/Home.vue')
-    }
-
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue'),
+  },
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
