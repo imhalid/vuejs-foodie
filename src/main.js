c144f3cbec71b22fdbbe67f2634852ade221e8f6
imhalid <imhalid@icloud.com>
2023-04-02 02:49:56 +0300

    add axios and fetch data

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 import router from './router'
 import store from './store'
-import './style.css'
 
 createApp(App).use(router).use(store).mount('#app')
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx}', './index.html'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
