c2a4fd77426437a508927b5895b921e65da05d36
imhalid <imhalid@icloud.com>
2023-04-03 02:20:32 +0300

    added layout route

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import MealList from '../views/MealList.vue'
 
+import Guest from '../components/Guest.vue'
+import Layout from '../components/Layout.vue'
+import Home from '../views/Home.vue'
+import MealsByIngredients from '../views/MealsByIngredients.vue'
+import MealsByLetter from '../views/MealsByLetter.vue'
+import MealsByName from '../views/MealsByName.vue'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/letter/:letter?',
+        name: 'byLetter',
+        component: MealsByLetter,
+      },
+      {
+        path: '/name/:name?',
+        name: 'byName',
+        component: MealsByName,
+      },
+      {
+        path: '/ingredients/:ingredients?',
+        name: 'byIngredient',
+        component: MealsByIngredients,
+      },
+    ],
   },
   {
-    path: '/letter/:letter',
-    name: 'byLetter',
-    component: MealList,
+    path: '/guest',
+    name: 'guest',
+    component: Guest,
   },
 ]
 
